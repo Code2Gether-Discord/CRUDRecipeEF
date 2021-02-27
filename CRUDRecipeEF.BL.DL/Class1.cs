d6d93cb195734061e7912b204417ed38debd69e6
chris <christianhunter707@gmail.com>
2021-02-27 20:39:48 +0100

    Add project files.

---
+++ CRUDRecipeEF.BL.DL/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CRUDRecipeEF.BL.DL
+{
+    public class Class1
+    {
+        //this is our business layer (logic) and data layer (database and entities)
+    }
+}
---
+++ CRUDRecipeEF.PL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CRUDRecipeEF.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //presentation layer (UI)
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
