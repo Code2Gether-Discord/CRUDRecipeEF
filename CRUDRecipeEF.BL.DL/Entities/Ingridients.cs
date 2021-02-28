210fac8756f5b5044a50dd428c3cfbdb18ba5286
Miguel Bastos <miguelb1397@gmail.com>
2021-02-28 17:10:00 +0000

    push

---
+++ CRUDRecipeEF.BL.DL/Entities/Ingridients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL
+{
+    public class Ingridients
+    {
+        public int IngridientID { get; set; }
+
+        public List<Recipes> Recipes { get; set; }
+        public string IngridientName { get; set; }
+
+    }
+}
---
+++ CRUDRecipeEF.BL.DL/Entities/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL
+{
+    public class Recipes
+    {
+        public int RecipeID { get; set; }
+
+        public List<Ingridients> Ingridients { get; set; }
+        public string RecipeName { get; set; }
+
+        
+    }
+}
