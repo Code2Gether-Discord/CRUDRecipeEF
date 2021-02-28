6d3cb4ee296818273cefe927ae1b519fb0e320ec
chris <christianhunter707@gmail.com>
2021-02-28 19:28:46 +0100

    fixed namespaces in entities

--- CRUDRecipeEF.BL.DL/Entities/Recipe.cs
+++ CRUDRecipeEF.BL.DL/Entities/Recipe.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace CRUDRecipeEF.BL.DL
+namespace CRUDRecipeEF.BL.DL.Entities
 {
     public class Recipe
     {
