3e7f5830228e1b55da869a51904d97bcb8ac1699
chris <christianhunter707@gmail.com>
2021-02-28 19:27:24 +0100

    interfaces for services created

---
+++ CRUDRecipeEF.BL.DL/Services/IRecipeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.Services
+{
+    public interface IRecipeService
+    {
+        Recipe GetRecipeByName(string name);
+        IEnumerable<Recipe> GetAllRecipes();
+        string AddRecipe(Recipe recipe);
+        string AddIngredientToRecipe(Ingredient ingredient, string recipeName);
+        void RemoveIngredientFromRecipe(string ingredientName, string recipeName);
+        void DeleteRecipe(string name);
+    }
+}
