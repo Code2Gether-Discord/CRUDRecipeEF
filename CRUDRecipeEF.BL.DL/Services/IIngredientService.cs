95ef84117a9c1b8f3364ea13fb58ebf197c476ad
chris <christianhunter707@gmail.com>
2021-02-28 19:27:24 +0100

    interfaces for services created

---
+++ CRUDRecipeEF.BL.DL/Services/IIngredientService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CRUDRecipeEF.BL.DL.Services
+{
+    public interface IIngredientService
+    {
+        Ingredient GetIngredientByName(string name);
+        IEnumerable<Ingredient> GetAllIngredients();
+        string AddIngredient(Ingredient ingredient);
+        void DeleteIngredient(string name);
+    }
+}
