7087dd33f098e33120055f954239288786655339
Kyle Givler <xvegan88x@gmail.com>
2021-03-06 16:29:53 -0500

    Add IngredientService

---
+++ CRUDRecipeEF.BL.DL/Services/IngredientService.cs
@@ -0,0 +1,32 @@
+﻿using CRUDRecipeEF.BL.DL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.Services
+{
+    class IngredientService : IIngredientService
+    {
+        public Task<string> AddIngredient(IngredientAddDTO ingredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteIngredient(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IngredientDetailDTO>> GetAllIngredients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IngredientDetailDTO> GetIngredientByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
