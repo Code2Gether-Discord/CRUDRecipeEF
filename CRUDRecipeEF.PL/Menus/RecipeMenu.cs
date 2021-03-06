428a896f04812d49f1c64a43a110f1cbcf532720
Kyle Givler <xvegan88x@gmail.com>
2021-03-06 15:12:15 -0500

    Add the basic guts

--- CRUDRecipeEF.PL/Menus/RecipeMenu.cs
+++ CRUDRecipeEF.PL/Menus/RecipeMenu.cs
@@ -19,6 +19,64 @@ namespace CRUDRecipeEF.PL.Menus
             Console.WriteLine();
             ConsoleHelper.ColorWriteLine(ConsoleColor.Red, "3.) Back to Main Menu");
             Console.WriteLine();
+
+            string input = string.Empty;
+            int option = 0;
+            bool valid = false;
+
+            while (!valid)
+            {
+                ConsoleHelper.ColorWrite(ConsoleColor.Yellow, "Please select an option: ");
+                input = Console.ReadLine();
+
+                valid = validateInt(input, 1, 3, out option);
+
+                if (!Enum.IsDefined(typeof(RecipeMenuOption), option))
+                {
+                    // Not in the enum - log here if desired
+                    valid = false;
+                }
+
+            }
+
+            RecipeMenuOption choice = (RecipeMenuOption)option;
+            ExecuteMenuSelection(choice);
+        }
+
+        private void ExecuteMenuSelection(RecipeMenuOption option)
+        {
+            switch (option)
+            {
+                case RecipeMenuOption.InValid:
+                    break;
+                case RecipeMenuOption.NewRecipe:
+                    break;
+                case RecipeMenuOption.LookUpRecipe:
+                    break;
+                case RecipeMenuOption.ShowRecipe:
+                    break;
+                case RecipeMenuOption.DeleteRecipe:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private bool validateInt(string input, int min, int max, out int result)
+        {
+            if (!int.TryParse(input, out result))
+            {
+                // Not a valid int - log error here if desired
+                return false;
+            }
+
+            if (result > max || result < min)
+            {
+                // Outside expected range - log error here if desired
+                return false;
+            }
+
+            return true;
         }
     }
 }
