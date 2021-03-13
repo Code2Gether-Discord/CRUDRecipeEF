7c5655638abe8e01e704884f33e6a73b00710e88
Kyle Givler <xvegan88x@gmail.com>
2021-03-13 12:52:45 -0500

    Add test project

---
+++ CRUDRecipeTests/IngredientServiceTests.cs
@@ -0,0 +1,35 @@
+using CRUDRecipeEF.BL.DL.Data;
+using CRUDRecipeEF.BL.DL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace CRUDRecipeTests
+{
+    public abstract class IngredientServiceTests
+    {
+        protected DbContextOptions<RecipeContext> ContextOptions { get; }
+
+        protected IngredientServiceTests(DbContextOptions<RecipeContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using (var context = new RecipeContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var apple = new Ingredient { Name = "Apple" };
+                var orange = new Ingredient { Name = "Orange" };
+                var peach = new Ingredient { Name = "Peach" };
+
+                context.AddRange(apple, orange, peach);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CRUDRecipeTests/SQLiteIngredientServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CRUDRecipeEF.BL.DL.Data;
+using CRUDRecipeEF.BL.DL.DTOs;
+using CRUDRecipeEF.BL.DL.Entities;
+using CRUDRecipeEF.BL.DL.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CRUDRecipeTests
+{
+    public class SQLiteIngredientServiceTests : IngredientServiceTests
+    {
+        private MapperConfiguration autoMapperConfig;
+
+        public SQLiteIngredientServiceTests() : 
+            base(new DbContextOptionsBuilder<RecipeContext>().UseSqlite("Filename=Test.db")
+                .Options)
+        {
+            autoMapperConfig = new MapperConfiguration(cfg => cfg.CreateMap<Ingredient, IngredientDetailDTO>());
+        }
+
+        [Fact]
+        public async Task Test_GetIngredientByName()
+        {
+            using (var context = new RecipeContext(ContextOptions))
+            {
+                var ingredientService = new IngredientService(context, new Mapper(autoMapperConfig));
+
+                var test = await ingredientService.GetAllIngredients();
+                var ing = await ingredientService.GetIngredientByName("Apple");
+                Assert.NotNull(ing);
+            }
+        }
+    }
+}
