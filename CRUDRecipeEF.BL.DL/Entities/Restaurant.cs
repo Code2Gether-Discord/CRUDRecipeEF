f01e79b6f30a57739ac421071639e3db9fedfd97
Giannis <75850486+Capetanios@users.noreply.github.com>
2021-03-14 13:30:39 +0200

    #30 #31

---
+++ CRUDRecipeEF.BL.DL/Entities/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Menu> Menus { get; set; } = new List<Menu>();
+    }
+}
