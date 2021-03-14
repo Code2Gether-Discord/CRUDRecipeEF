30890cc3e57807f21a6182874f395ed4edae38f5
chris <christianhunter707@gmail.com>
2021-03-14 14:25:45 +0100

    irestaurant service

---
+++ CRUDRecipeEF.BL.DL/Services/IRestaurantService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUDRecipeEF.BL.DL.Entities;
+
+namespace CRUDRecipeEF.BL.DL.Services
+{
+    public interface IRestaurantService
+    {
+        Task AddRestaurant(Restaurant restaurant);
+        Task DeleteRestaurant(Restaurant restaurant);
+        Task DeleteMenu(string name);
+        Task<Restaurant> GetRestaurantByName(string name);
+        Task<string> AddMenu(Menu menu);
+    }
+}
