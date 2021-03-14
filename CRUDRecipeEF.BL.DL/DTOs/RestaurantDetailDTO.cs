fd3b97ed86ce95c30551063d025f609e359a8a6b
chrisK00 <72318023+chrisK00@users.noreply.github.com>
2021-03-14 22:44:20 +0100

    Maybe it is correct

---
+++ CRUDRecipeEF.BL.DL/DTOs/RestaurantDetailDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.DTOs
+{
+    public class RestaurantDetailDTO
+    {
+        public string Name { get; set; }
+        public List<RestaurantDetailDTO> Restaurants { get; set; } = new List<RestaurantDetailDTO>();
+    }
+}
--- CRUDRecipeEF.BL.DL/Helpers/AutoMapperProfiles.cs
+++ CRUDRecipeEF.BL.DL/Helpers/AutoMapperProfiles.cs
@@ -15,6 +15,10 @@ namespace CRUDRecipeEF.BL.DL.Helpers
             CreateMap<Ingredient, IngredientDetailDTO>();
             CreateMap<IngredientAddDTO, Ingredient>()
                 .ForMember(i => i.Name, opt => opt.MapFrom(i => i.Name.Trim()));
+
+            CreateMap<Restaurant, RestaurantDetailDTO>();
+            CreateMap<RestaurantAddDTO, Restaurant>()
+                .ForMember(i => i.Name, opt => opt.MapFrom(i => i.Name.Trim()));
         }
     }
 }
