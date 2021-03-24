38ca9945879da150305cc9c6af612015de5384d6
Miguel Bastos <miguelb1397@gmail.com>
2021-03-24 08:42:24 +0000

    Added DTO

---
+++ CRUDRecipeEF.BL.DL/DTOs/RecipeCategoryAddDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.DTOs
+{
+    class RecipeCategoryAddDTO
+    {
+        [StringLength(200, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public List<RecipeAddDTO> Recipes { get; set; } = new List<RecipeAddDTO>();
+    }
+}
---
+++ CRUDRecipeEF.BL.DL/DTOs/RestaurantAddDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDRecipeEF.BL.DL.DTOs
+{
+    public class RestaurantAddDTO
+    {
+        [StringLength(200, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public List<MenuAddDTO> Menus { get; set; } = new List<MenuAddDTO>();
+    }
+}
