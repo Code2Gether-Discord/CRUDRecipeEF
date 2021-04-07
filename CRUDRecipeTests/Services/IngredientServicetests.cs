14e109d53dbfc72dc2c20469d096ba93748f4a42
chris <christianhunter707@gmail.com>
2021-04-07 22:29:52 +0200

    setup servicetest class

--- CRUDRecipeTests/Services/IngredientServicetests.cs
+++ CRUDRecipeTests/Services/IngredientServicetests.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using CRUDRecipeEF.BL.Services;
+using CRUDRecipeEF.DAL.Helpers;
+using CRUDRecipeEF.DAL.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace CRUDRecipeTests.Services
 {
-    class IngredientServicetests
+    public class IngredientServicetests
     {
+        public readonly Mock<IIngredientRepo> _mockRepo;
+        private readonly Mock<IUnitOfWork> _UoW;
+        private readonly MapperConfiguration _mapperConfig;
+        private readonly IIngredientService _ingredientService;
+        private readonly Mock<ILogger<IngredientService>> _logger;
+
+        public IngredientServicetests()
+        {
+            //mock the repo against the interface
+            _mockRepo = new Mock<IIngredientRepo>();
+            _UoW = new Mock<IUnitOfWork>();
+            _logger = new Mock<ILogger<IngredientService>>();
+
+            _mapperConfig = new MapperConfiguration(c => c.AddProfile<AutoMapperProfiles>());
+
+            _ingredientService = new IngredientService(_mockRepo.Object, new Mapper(_mapperConfig),
+               _logger.Object, _UoW.Object);
+        }
+
+        public void SetupData()
+        {
+        }
     }
 }
